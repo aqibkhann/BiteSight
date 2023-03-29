f78b22d44da599729351f1cdfe2b0458d8463c5c
aqibkhann <aqibkhan1911@gmail.com>
2023-03-29 12:20:33 +0530

    Fix styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,19 @@ module.exports = {
   mode: "jit",
   theme: {
     extend: {
+      keyframes: {
+        'bounce-slow': {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-25%)' },
+        },
+      },
+      animation: {
+        'bounce-slow': 'bounce-slow 3s infinite', // Adjust the duration here
+      },
+      transitionDelay: {
+        300: '300ms',
+        600: '600ms',
+      },
       colors: {
         primary: "#00040f",
         secondary: "#00f6ff",
@@ -15,12 +28,12 @@ module.exports = {
       },
     },
     screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
+      xs: "500px",
+      ss: "1200px",
+      sm: "1200px",
+      md: "1200x",
       lg: "1200px",
-      xl: "1700px",
+      xl: "1500px",
     },
   },
   plugins: [],
